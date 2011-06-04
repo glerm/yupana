e29703fa83f269ed261bd35ef2849f7f2f1759aa
Glerm Soares <organismo@gmail.com>
2011-06-04 00:19:31 -0300

    script bot yupana_irc.py funciona

--- python/yupana_irc.py
+++ python/yupana_irc.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 import sys
 import socket
@@ -6,19 +7,19 @@ import string
 
 HOST="irc.freenode.net"
 PORT=6667
-NICK="asdf"
-IDENT="asdf"
-REALNAME="asdf"
-CHAN="#asdf"
-readbuffer=""
+NICK="yupana"
+IDENT="kernelpanic"
+REALNAME="yupana"
+CHAN="#tupantapuia"
+readbuffer="عبور الحدود aquilo que dissolve no solo e continua a jornada electroquímica dos corpos finalmente será separada daquilo que sempre esteve apenas permeando – um dicionário em aglutinação movendo aqueles braços, pernas, cabeça, tripas. esta árvore de sinais, continua se espalhando e entidades que חציית הגבול"
 
 s=socket.socket( )
 s.connect((HOST, PORT))
 s.send("NICK %s\r\n" % NICK)
 s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
 s.send("JOIN :%s\r\n" % CHAN)
-s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Hello There!"))
-s.send("PRIVMSG %s :%s\r\n" % (CHAN, "I am a bot"))
+s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Ola"))
+s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Eu sou Yupana. Meu código esta em https://github.com/glerm/yupana"))
 
 while 1:
     readbuffer=readbuffer+s.recv(1024)
